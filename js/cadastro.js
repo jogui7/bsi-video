eb04a46adf84409514c7eeb32150269f47b2a5cf
ArthurHenrique-Reis <carvalhoarthurreis@gmail.com>
2021-04-08 11:44:20 -0300

    Tela de Cadastro parte html e css

---
+++ js/cadastro.js
@@ -0,0 +1,38 @@
+
+function cadastrar(){
+    var nome = $("#txtNome").val();
+    var nascimento = $("#txtNascimento").val();
+    var email =   $("#txtEmail").val();
+    var senha = $("#Senha").val();
+    var confsenha = $("#ConfSenha").val();
+    var numero = $("#txtNumero").val();
+    var validade = $("#txtValidade").val();
+    var codigo = $("#txtCodigo").val();
+    var titular = $("#txtTitular").val();
+    var CPF = $("#txtCPF").val();
+
+    alert("A");
+        
+    var formData = {};
+    formData.append('name', nome);
+    formData.append('birthdate', mysqlDate()  );
+    formData.append('email', email );
+    formData.append('password', senha );
+    formData.append('creditCardNumber', numero );
+    formData.append('creditCardExpireDate', mysqlDate() );
+    formData.append('ccv', codigo );
+    formData.append('cardHolderName', titular );
+    formData.append('cpfCNPJ', CPF );
+
+    fetch("http://localhost/bsi-video/api/users/index.php/", {
+        method: "POST",
+        body: formData
+    })
+}
+
+function mysqlDate(date){
+    date = date || new Date();
+    return date.toISOString().split('T')[0];
+}
+
+
