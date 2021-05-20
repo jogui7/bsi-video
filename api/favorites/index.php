b12447eeea1339ce7e33bd40263e549e1f3f6e01
João Guis <joao.guis2001@gmail.com>
2021-05-20 00:27:49 -0300

    Adicionado favoritar filme

--- api/favorites/index.php
+++ api/favorites/index.php
@@ -1,12 +1,13 @@
 <?php
 
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: PATCH, POST, GET, DELETE");
 header("Access-Control-Allow-Origin: http://bsi.video.test");
 header("Access-Control-Allow-Credentials: true");
 
 session_start();
+$method = $_SERVER['REQUEST_METHOD'];
 
-if (!isset($_SESSION['userId'])) {
+if (!isset($_SESSION['userId']) && $method != 'OPTIONS') {
     echo json_encode(array('message'=>'Acesso negado'));
     return http_response_code(403);
     exit;
@@ -18,27 +19,26 @@ require_once './favorites.class.php';
 
 $favorites = new Favorites();
 
-$method = $_SERVER['REQUEST_METHOD'];
 // $request = $_SERVER['QUERY_STRING'];
 
 if(isset($_SERVER['PATH_INFO'])) {
     $request = explode("/", $_SERVER['PATH_INFO']);
+} else {
+    $request = "";
 }
-
-$request = "";
 
 switch ($method) {
     case 'PATCH':
         $favorites->update($mysqli);  
         break;
     case 'POST':
-        $favorites->create($mysqli);  
+        $favorites->create($mysqli, $request);  
         break;
     case 'GET':
         $favorites->find($mysqli); 
         break;
     case 'DELETE':
-        $favorites->delete($mysqli); 
+        $favorites->delete($mysqli, $request); 
         break;
     default:
         handle_error($request);  
--- js/filme.js
+++ js/filme.js
@@ -33,7 +33,8 @@ function logout(){
 
 function fetchFilmes() {
     var params = new window.URLSearchParams(window.location.search);
-    fetch(`http://localhost/bsi-video/api/movies/index.php/${params.get('id')}`, {
+    const movieId = params.get('id');
+    fetch(`http://localhost/bsi-video/api/movies/index.php/${movieId}`, {
         credentials:"include"
     })
     .then(async (res) => {
@@ -68,7 +69,7 @@ function fetchFilmes() {
                         <div class="row-item">
                             <b>Descrição:</b>
                             <p>${filme.synopsis}</p>
-                            <button>Adicionar aos favoritos</button>
+                            <button id="favbtn">Adicionar aos favoritos</button>
                         </div>
                     </div>
                 </div>
@@ -77,6 +78,34 @@ function fetchFilmes() {
             <b>Trailer:</b>
             <iframe src="${filme.trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         `);
+    }).then(() => {
+        fetch(`http://localhost/bsi-video/api/favorites/index.php/`, {
+            credentials:"include"
+        })
+        .then(async (res) => {
+            const favs = await res.json();
+            if(!res.ok) {
+                return;
+            }
+            $('#favbtn').text(favs.map(fav => fav.id).includes(movieId) ? 'Remover dos favoritos' : 'Adicionar aos favoritos');
+            $('#favbtn').click(function() { 
+                handleFav(movieId) 
+            });
+        })
+    })
+}
+
+function handleFav(movieId) {
+    fetch(`http://localhost/bsi-video/api/favorites/index.php/${movieId}`, {
+        method: $('#favbtn').text() == 'Adicionar aos favoritos' ? 'POST' : 'DELETE',
+        credentials:"include"
+    })
+    .then(async (res) => {
+        if(res.ok) {
+            $('#favbtn').text($('#favbtn').text() == 'Adicionar aos favoritos' ? 'Remover dos favoritos' : 'Adicionar aos favoritos');
+            return;
+        } 
+        alert(`Erro ao ${$('#favbtn').text() == 'Remover dos favoritos' ? 'remover dos favoritos' : 'adicionar aos favoritos'}`);
     })
 }
 
